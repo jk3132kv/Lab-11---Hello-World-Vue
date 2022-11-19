e170829eff9c58e7982d7693cdc1f8f53facba3e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-18 21:07:56 -0600

    Lab 11

---
+++ routes/routes.js
@@ -0,0 +1,8 @@
+let express = require('express')
+let router = express.Router()
+
+router.get('/', function(req, res, next){
+    res.json( { 'message': 'Hello I am an Azure app!'} )
+})
+
+module.exports = router
